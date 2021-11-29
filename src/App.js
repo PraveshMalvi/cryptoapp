7d8f6097cb366881e12dd605f91ec89b04f05c41
Pravesh <praveshmalvi009@gmail.com>
2021-11-30 00:16:08 +0530

    Adding all the files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./style.css";
+import Crypto from "./cryptoApp/Crypto";
+
+function App() {
+  return (
+    <>
+      <Crypto />
+    </>
+  );
+}
+
+export default App;
